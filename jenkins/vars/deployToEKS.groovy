ffcddcf77e503dca9f06da250d97a24c936049e9
Oswald Gyabaah <oswald.gyabaah@gtbank.com>
2025-04-30 07:56:06 +0000

    Refactor cluster name to make it a variable

--- jenkins/vars/deployToEKS.groovy
+++ jenkins/vars/deployToEKS.groovy
@@ -1,4 +1,4 @@
-def call(String serviceName, String imageTag) {
+def call(String clusterName, String serviceName, String imageTag) {
     def helmChartPath = "kubernetes/helm/${serviceName}"
     def imageRepo = "${env.ECR_REGISTRY}/${serviceName}"
 
@@ -7,7 +7,7 @@ def call(String serviceName, String imageTag) {
         credentialsId: 'aws-keys'
     ]]) {
         sh """
-            aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name $EKS_CLUSTER_NAME
+            aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name $clusterName
 
             helm upgrade --install ${serviceName} ${helmChartPath} \
                 --namespace default \
